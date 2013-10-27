bcee23fe36f6f93fa230225938563663f2616143
Markov Alexey <piton4eg@mail.ru>
2013-10-27 21:48:43 +0400

    Two clients

--- main.go
+++ main.go
@@ -1,46 +1,81 @@
 package main
-
+ 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"sync"
 )
-
-func handleClient(conn net.Conn) {
+ 
+type Message struct {
+	conn *net.TCPConn
+	msg  string
+}
+ 
+var (
+	messages     = make(chan Message)
+	connections  = make(map[string]*net.TCPConn)
+	connectionsM sync.RWMutex
+)
+ 
+func handleClient(conn *net.TCPConn) {
 	defer func() {
-		log.Printf("%v logout", conn.RemoteAddr())
+		log.Printf("Disconnect from %v", conn.RemoteAddr())
 		conn.Close()
+		connectionsM.Lock()
+		delete(connections, conn.RemoteAddr().String())
+		connectionsM.Unlock()
 	}()
-
-	log.Printf("Connection from %v", conn.RemoteAddr())
-
+ 
+	log.Printf("Connect from %v", conn.RemoteAddr())
+ 
 	buf := make([]byte, 1024)
 	for {
 		n, err := conn.Read(buf)
 		if n > 0 {
-			log.Printf("%s", buf[:n])
+			messages <- Message{conn, string(buf[:n])}
 		}
 		if err != nil {
 			if err != io.EOF {
-				log.Print(err)
+				log.Printf("Error from %v: %s", conn.RemoteAddr(), err)
 			}
 			return
 		}
-
 	}
 }
-
+ 
 func main() {
+	go func() {
+		for m := range messages {
+			connectionsM.RLock()
+			for _, conn := range connections {
+				if m.conn != conn {
+					s := fmt.Sprintf("%s: %s", m.conn.RemoteAddr(), m.msg)
+					_, err := conn.Write([]byte(s))
+					if err != nil {
+						log.Print(err)
+					}
+				}
+			}
+			connectionsM.RUnlock()
+		}
+	}()
+ 
 	ln, err := net.Listen("tcp", ":12345")
 	if err != nil {
 		log.Fatal(err)
 	}
 	for {
-		conn, err := ln.Accept()
+		c, err := ln.Accept()
 		if err != nil {
 			log.Print(err)
 		}
-
+ 
+		conn := c.(*net.TCPConn)
+		connectionsM.Lock()
+		connections[conn.RemoteAddr().String()] = conn
+		connectionsM.Unlock()
 		go handleClient(conn)
 	}
 }
